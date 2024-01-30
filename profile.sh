6d6dc0cc71808f64f6d0de1fa6b60669661230d7
David Buschman <dave.buschman@qalocate.com>
2024-01-30 15:16:52 -0600

    added full auto support in the profile shell script

--- profile.sh
+++ profile.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
+set -ex
 
-set -e
+MODULE_OUT="httpsrvimg"
+JLINK_BIN="$(pwd)/${MODULE_OUT}/bin/java"
+
+WRK_BUILD="wrk_build"
+WRK_BIN="$(pwd)/${WRK_BUILD}/wrk"
 
 getCurrentTime() {
     date +"%s.%3N"
 }
 
-runHttpServer() {
-    ./httpsrvimg/bin/java -Dsun.net.httpserver.nodelay=true -m httpsrv/httpsrv.Hello
+buildJavaModule() { 
+   echo "Build Java App"
+   rm -fr $MODULE_OUT 
+   javac -d mods/ src/httpsrv/*.java src/module-info.java
+   jlink --module-path $JAVA_HOME/jmods:mods --add-modules httpsrv --output $MODULE_OUT
+   ls -la $JLINK_BIN
+} 
+
+buildWrk() { 
+   echo "Build wrk load tester"
+   if [[ -e "$WRK_BUILD" ]]; then 
+     echo "Wrk appears to already be available"
+     return 0
+   fi 
+
+   git clone https://github.com/wg/wrk.git $WRK_BUILD
+   cd $WRK_BUILD
+   make
 }
+
+runHttpServer() {
+    $JLINK_BIN -Dsun.net.httpserver.nodelay=true -m httpsrv/httpsrv.Hello
+} 
 
 profileHttpServer() {
     read line # wait for "ready" line emitted by server
@@ -25,6 +50,7 @@ makeCurlRequest() {
 }
 
 measureExecutionTimes() {
+    echo "Run Simple tests" 
     for i in {1..10}; do
         local startTime=$(getCurrentTime)
         runHttpServer | profileHttpServer $startTime
@@ -44,4 +70,20 @@ processTimings() {
     }'
 }
 
-measureExecutionTimes
+loadTest() {
+  read line # wait for "ready" line emitted by server
+  echo "Run load test" 
+  $WRK_BIN --latency -d 60s -c 100 -t 8 http://localhost:8080/ 
+  pkill java
+} 
+
+
+# MAIN
+# ##############################
+ buildWrk
+ buildJavaModule
+# measureExecutionTimes
+ runHttpServer | loadTest
+# ##############################
+
+echo "DONE"
